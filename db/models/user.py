12f1faadffea5aef2aa42f0ab39590059ec6d672
dostontv <dostontv969@gmail.com>
2024-07-28 14:44:35 +0500

    Fixed models added User model

--- db/models/user.py
+++ db/models/user.py
@@ -5,11 +5,5 @@ from db.base import CreatedModel
 
 
 class User(CreatedModel):
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
-    first_name: Mapped[str] = mapped_column(VARCHAR(255))
-    last_name: Mapped[str] = mapped_column(VARCHAR(255), nullable=True)
-    email: Mapped[str] = mapped_column(VARCHAR(255), unique=True, nullable=True)
-    password: Mapped[str] = mapped_column(VARCHAR(255), nullable=True)
-
-    username: Mapped[str] = mapped_column(VARCHAR(255), nullable=True)
-    phone_number: Mapped[str] = mapped_column(VARCHAR(255), nullable=True)
+    user_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    name: Mapped[str] = mapped_column(VARCHAR)
