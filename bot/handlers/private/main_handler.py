9edbe7476c96a8390fb1e11ff1e3b637539372b9
Sardor23324 <deo17ssssssssss@gmail.com>
2024-07-28 16:31:59 +0500

    database hato

--- bot/handlers/private/main_handler.py
+++ bot/handlers/private/main_handler.py
@@ -9,5 +9,12 @@ main_router = Router()
 
 @main_router.message(CommandStart())
 async def command_start_handler(message: Message, bot: Bot) -> None:
-    user_data = message.from_user.model_dump(include={'user_id', 'full_name'})
+    user_data = message.from_user.model_dump(include={'user_id', 'first_name'})
+    if  User.get(message.from_user.id) is None:
+        User.create(id = user_data.get("id") , name = user_data.get("first_name"))
+    else:
+        await message.answer(f"""Assalomu alaykum {message.from_user.first_name} 🤖
+        Tarjimalar Tv Bot - orqali siz o'zingizga yoqqan kinoni topishingiz mumkin 🎬
+        Shunchaki kino kodini yuboring va kinoni oling ✅""")
+
 
